a4a19f03621de63ca73f85caaebcde5d692bbc4e
eeg1412 <eeg1412@gmail.com>
2023-12-13 18:06:25 +0900

    测试

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   routeRules: {
-    '/api/**': { proxy: 'https://card.wikimoe.com/api/**' },
+    '/api/**': { proxy: 'http://127.0.0.1:3001/api/**' },
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
--- pages/index22.vue
+++ pages/index22.vue
@@ -4,10 +4,14 @@
     <p>This is the index page.</p>
     <div>{{ typeof data }}：</div>
     <div>{{ data }}</div>
-    <div></div>
+    <div @click="clickReflush">刷新</div>
   </div>
 </template>
 <script setup>
 const { data } = await useFetch('/api/news', { method: 'POST' })
 console.log(data.value)
+const clickReflush = () => {
+  // 使用$fetch()
+  $fetch('/api/news', { method: 'POST' })
+}
 </script>
